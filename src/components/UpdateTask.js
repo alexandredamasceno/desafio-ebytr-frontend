d19f3f0aee6ae1250bcd8a34090e6b7aed58fa56
Alexandre Damasceno <ale.damasceno.silva@gmail.com>
2021-11-05 00:02:39 -0300

    Cria um arquivo para atualizar uma tarefa especifica

---
+++ src/components/UpdateTask.js
@@ -0,0 +1,59 @@
+import React, { useState } from 'react';
+
+function UpdateTask() {
+    const [task, setTask] = useState({
+        task: '',
+        status: 'Pendente', 
+     });
+ 
+     const setInputs = ({ target }) => {
+         setTask({ ...task, [target.name]: target.value });
+     };
+ 
+     const submitInputs = () => {
+         const url = 'http://localhost:3001/tasks';
+         const header = {
+             method: 'PUT',
+             body: JSON.stringify(task),
+             headers: new Headers({
+                 'Content-Type': 'application/json',
+                 'Accept': 'application/json',
+             })
+         };
+         fetch(url, header)
+             .catch((err) => console.log('Erro aqui', err));
+     };
+    return (
+        <div>
+            <header>
+                <h2>Atualizar tarefa</h2>
+            </header>
+            <div>
+                <form>
+                    <input
+                    type="text"
+                    name="task"
+                    placeholder="Digite aqui sua nova tarefa"
+                    onChange={ setInputs }
+                    />
+                    {/* <br></br> */}
+                    {/* <label> */}
+                        {/* Status */}
+                        <select
+                        name="status"
+                        onChange={ setInputs }
+                        >
+                            <option value="Pendente">Pendente</option>
+                            <option value="Em andamento">Em andamento</option>
+                            <option value="Pronto">Pronto</option>
+                        </select>
+                    {/* </label> */}
+                    <br></br>
+                    <button type="button" onClick={ submitInputs }>Adicionar</button>
+                </form>
+            </div>
+        </div>
+    );
+};
+
+export default UpdateTask;
